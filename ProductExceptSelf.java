8cf765588dc5ebc05a638b5ec0dd0ba15f040a86
Harshad Tile <155941730+HarshadTile@users.noreply.github.com>
2025-01-28 23:06:52 +0530

    Create ProductExceptSelf.java
    productExceptSelf

---
+++ ProductExceptSelf.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+     int n= nums.length;
+    //  int []prefix = new int[n];
+    //  int []suffix = new int[n];
+     int []ans = new int[n];
+    // int prefix = 1 ;
+     int suffix =1;
+     ans[0]=1;
+     for(int i=1;i<n;i++){
+        // prefix[i] = prefix[i-1] * nums[i-1];//1,2,
+        ans[i] = ans[i-1]* nums[i-1];
+     }
+     for(int i=n-2;i>=0;i--){
+        
+        suffix *= nums[i+1];
+        ans[i] *= suffix;
+     }
+    return ans;
+    }
+}
