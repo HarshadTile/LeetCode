721a2531257c6ddced0ac6a51623d662d08ce786
Harshad Tile <155941730+HarshadTile@users.noreply.github.com>
2025-02-28 16:16:08 +0530

    Create NumberOfSteps.java
    Null

---
+++ NumberOfSteps.java
@@ -0,0 +1,21 @@
+class NumberOfSteps {
+    
+    public int numberOfSteps(int num) {
+
+      return helper(num,0);
+        
+    }
+    private static int helper(int num,int c){
+          if(num==0){
+            return c;
+        }
+
+        if(num%2==0)
+            return helper(num/2,c+1);
+        
+        return helper(num-1,c+1);
+    }
+
+
+    
+}
